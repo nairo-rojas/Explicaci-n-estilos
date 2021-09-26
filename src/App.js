fc996737861e2a26bf3cdfe8832146859ca6a4c4
nairo-rojas <nairo.rojas@gmail.com>
2021-09-26 00:37:10 -0500

    explicación de css con la libreria tailwind

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 function App() {
   return (
     <div className="App">
-      <spam classNameName="forndoOscuro">Hola mundo</spam>
-      <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <form>
-      <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-      </div>
-      <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1"></input>
-      </div>
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
-        <label className="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+     <div className="flex flex-col md:flex-row">
+     <div className="mx-3 bg-gray-400">Elemento 1</div>
+     <div className="mx-3 bg-red-300">Elemento 2</div>
+     <div className="mx-3 bg-green-600">Elemento 3</div>
+     <div className="mx-3 bg-blue-400">Elemento 4</div>
+     <button className="boton">Clic aqui</button>
+     <button className="boton">Clic aqui</button>
+     </div>
     </div>
   );
 }
