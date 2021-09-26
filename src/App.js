73f6bea1387116a093c7920d63b8f050357fca3b
nairo-rojas <nairo.rojas@gmail.com>
2021-09-25 21:03:53 -0500

    agregar estilos con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
     <div className="App">
-      <spam>Hola mundo</spam>
+      <spam classNameName="forndoOscuro">Hola mundo</spam>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <form>
+      <div className="mb-3">
+        <label for="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div className="mb-3">
+        <label for="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1"></input>
+      </div>
+      <div className="mb-3 form-check">
+        <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
     </div>
   );
 }
